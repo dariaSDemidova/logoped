68b24383b0cdf047a4a15b3eccdcbbf0cf35a46c
Alena Elhel <ochumenno66@yandex.ru>
2025-08-19 15:56:00 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+/* Header burger menu */
+const burger = document.querySelector(".header__burger");
+const navList = document.querySelector(".header__nav-list");
+const navLinks = document.querySelectorAll(".header__nav-list a");
+
+// Клик по бургеру
+burger.addEventListener("click", () => {
+  burger.classList.toggle("open");
+  navList.classList.toggle("open");
+});
+
+// Клик по любой ссылке меню
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    burger.classList.remove("open");
+    navList.classList.remove("open");
+  });
+});
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const modal = document.getElementById("privacy-modal");
   const openBtn = document.querySelector(".open-modal");
