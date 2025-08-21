98d4c3ca8d5500d258a25d46be3025122d7338a3
Darya Semina <darina1970@mail.ru>
2025-08-21 13:33:48 +0700

    feat: add a hover and puzzle changes

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (entry.isIntersecting) {
           setTimeout(() => {
             dino.classList.add("visible");
-          }, 800);
+          }, 500);
           observer.unobserve(section);
         }
       });
