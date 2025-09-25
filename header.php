99a2c07883b4cdbb7b36c72ca4d5cfbb8969eec9
Daria Demidova <daashenkaa@mail.ru>
2025-09-25 13:55:47 +0300

    feat: add yandex metrika

--- header.php
+++ header.php
@@ -8,6 +8,29 @@
             echo " | ";
             bloginfo('description'); ?></title>
 
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0],
+                k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=104117922", "ym");
+
+        ym(104117922, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/104117922" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
+
     <?php
     wp_head();
     ?>
